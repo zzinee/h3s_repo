c86fe49de8f11d8419d7a22f8191038caa49f0fd
vagrant <vagrant@example.com>
2023-04-22 13:39:58 +0900

    main 수정 - khj

--- SampleProject/src/test/SampleTest.java
+++ SampleProject/src/test/SampleTest.java
@@ -4,7 +4,7 @@ public class SampleTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi sample");
+		System.out.println("hi sample - khj 수정");
 	}
 	public void saveProduct() {
 		System.out.println("sjhan 상품 저장");
